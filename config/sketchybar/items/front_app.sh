ebebd17d14d9046120b890b867f7aa498b56883f
Jacob Lauritzen <3637499+Jacse@users.noreply.github.com>
2025-09-05 18:28:58 +0200

    transparency

--- config/sketchybar/items/front_app.sh
+++ config/sketchybar/items/front_app.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env sh
 
-sketchybar --add       item         front_app left                    \
-           --set       front_app    script="$PLUGIN_DIR/front_app.sh" \
-                                    icon.drawing=off                  \
-                                    background.padding_right=48         \
-                                    label.color=0x99cad3f5                 \
-           --subscribe front_app    front_app_switched
-
+sketchybar --add item front_app left \
+  --set front_app script="$PLUGIN_DIR/front_app.sh" \
+  background.padding_right=48 \
+  --subscribe front_app front_app_switched
